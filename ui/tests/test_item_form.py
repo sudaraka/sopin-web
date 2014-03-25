2091a1bec16a93f8360d0ad13f26cacf3219ded6
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-03-25 19:07:36 +0530

    Unit test for item add form view

---
+++ ui/tests/test_item_form.py
@@ -0,0 +1,33 @@
+#  ui/tests/test_item_form.py: Unit tests for item add/edit form
+#
+#  Copyright 2014 Sudaraka Wijesinghe <sudaraka.org/contact>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+""" UI Unit test for item add/edit """
+
+from ui.tests.base import BaseUnitTestCase
+
+
+class ItemAddFormTest(BaseUnitTestCase):
+    """ Item add/edit form unit test """
+
+    uri = '/item-maintenance/form'
+    template = 'items/form.html'
+
+    def test_uri_render_correct_template(self):
+        """ Call base class function """
+
+        self.uri_render_correct_template()
