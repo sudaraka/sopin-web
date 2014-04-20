86d81b3b76d0a4899e6db933152b5a316261aca4
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-04-20 16:28:34 +0530

    Partly completed functional test for homepage right hand side column

--- ft/homepage/ui/test_elements.py
+++ ft/homepage/ui/test_elements.py
@@ -22,7 +22,13 @@ states.
 
 """
 
+import copy
+import datetime
+import random
+
 from ft.base import FunctionalTestBase
+
+from data.models.inventory import Item, Purchase
 
 
 class HomepageFirstVisit(FunctionalTestBase):
@@ -32,7 +38,7 @@ class HomepageFirstVisit(FunctionalTestBase):
 
     """
 
-    def test_elements_with_no_user_generated_content(self):
+    def xtest_elements_with_no_user_generated_content(self):
         """
         Verify all the required elements are available in homepage HTML.
 
@@ -71,3 +77,84 @@ class HomepageFirstVisit(FunctionalTestBase):
                          area2.find_element_by_css_selector('.running-out h3')
                          .text)
         self.assertIn('No items running out', area2.text)
+
+
+class HomepageVisitWithRunningOutItemData(FunctionalTestBase):
+    """
+    Test for homepage elements when user visit the site with the inventory
+    containing items that run out soon.
+
+    """
+
+    test_data = [
+        # Item without purchase
+        {'name': 'test item #1', },
+
+        # Item passed threshold
+        {'name': 'test item #2',
+         'last_purchase': datetime.date.today() - datetime.timedelta(22), },
+
+        # Item purchased few weeks ago (in threshold, running out)
+        {'name': 'test item #3',
+         'last_purchase': datetime.date.today() - datetime.timedelta(20), },
+
+        # Item purchased few days ago (in threshold, NOT running out)
+        {'name': 'test item #4',
+         'last_purchase': datetime.date.today() - datetime.timedelta(11), },
+        {'name': 'test item #5',
+         'last_purchase': datetime.date.today() - datetime.timedelta(9), },
+        {'name': 'test item #6',
+         'last_purchase': datetime.date.today() - datetime.timedelta(5), },
+
+        # Same data with non-default threshold
+        {'name': 'test item #7', 'purchase_threshold': 10,
+         'last_purchase': datetime.date.today() - datetime.timedelta(22), },
+
+        # Item purchased few weeks ago (in threshold, running out)
+        {'name': 'test item #8', 'purchase_threshold': 10,
+         'last_purchase': datetime.date.today() - datetime.timedelta(20), },
+
+        # Item purchased few days ago (in threshold, NOT running out)
+        {'name': 'test item #9', 'purchase_threshold': 10,
+         'last_purchase': datetime.date.today() - datetime.timedelta(11), },
+        {'name': 'test item #10', 'purchase_threshold': 10,
+         'last_purchase': datetime.date.today() - datetime.timedelta(9), },
+        {'name': 'test item #11', 'purchase_threshold': 10,
+         'last_purchase': datetime.date.today() - datetime.timedelta(5), },
+    ]
+
+    def setUp(self):  # pylint: disable=I0011,E1002
+        """ Override parent method to populate context with Item data """
+
+        for item in self.test_data:
+            i = copy.copy(item)
+
+            if 'last_purchase' in i:
+                del i['last_purchase']
+
+            created_item = Item.objects.create(**i)
+
+            if 'last_purchase' in item:
+                Purchase.objects.create(item=created_item,
+                                        quantity=random.randrange(1, 11),
+                                        date=item['last_purchase'])
+
+        super(HomepageVisitWithRunningOutItemData, self).setUp()
+
+    def test_elements_in_right_column(self):
+        """
+        Verify all the required elements are available in homepage HTML.
+
+        """
+
+        # Shopper visit the site later with some items that run out soon
+        # already in the inventory.
+        #
+        # 1. Them message that was there before saying "no items" is now gone
+        area = self.browser.find_element_by_css_selector(
+            '.right-pane .running-out')
+        self.assertNotIn('No items running out', area.text)
+
+        #
+        # 2. The correct items are listed in the "Items Running Out" list.
+        self.assertTrue(False, 'FINISH FT')
