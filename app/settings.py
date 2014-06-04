1a8fafb170ac7759a456fb96d5baf2a39da03426
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-06-04 12:56:09 +0530

    Set the STATIC_ROOT for collectstatic command

--- app/settings.py
+++ app/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../var/www/')
 
 SITE_TITLE = 'Sopin'
 VERSION = (0, 1, 'alpha')
